89334132cd9bd46bacbcf6a48cff109f4ac55207
김용건 (Kim Younggeon) <mail@kyg.kr>
2021-03-31 17:15:42 +0900

    chore: `@nuxt/image` 모듈 타입 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,12 +18,12 @@ const nuxtConfig: NuxtConfig = {
   ],
   image: {
     screens: {
-      xs: '480px',
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      xxl: '1600px'
+      xs: 480,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1600
     }
   }
 }
